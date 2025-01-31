88ea03de5a509939e88de77c6fb8364edd21a3c0
Andrew Mason <andrew.mason@calendly.com>
2025-01-31 17:47:39 -0500

    add cpu profiling
    Signed-off-by: Andrew Mason <andrew.mason@calendly.com>

--- app/controllers/rails/prof/profile_controller.rb
+++ app/controllers/rails/prof/profile_controller.rb
@@ -1,8 +1,18 @@
 require "objspace"
+require "stackprof"
 
 module Rails::Prof
   class ProfileController < ApplicationController
     def index
+    end
+
+    def cpu
+      seconds = params.fetch(:seconds, 10).to_i
+      profile = StackProf.run(mode: :cpu, raw: true) do
+        sleep seconds
+      end
+
+      send_data Marshal.dump(profile), type: "text/plain", filename: "profile"
     end
 
     def heap
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails::Prof::Engine.routes.draw do
     root to: "profile#index"
 
+    get "/cpu", to: "profile#cpu"
     get "/heap", to: "profile#heap"
 end
